ae5320141dab2435c4dfe434ad220e30ee40a111
Jyle Vergara <jylevergara@gmail.com>
2024-03-05 11:51:42 -0500

    add struct word

--- WordAnalyzer.h
+++ WordAnalyzer.h
@@ -2,13 +2,19 @@
 #ifndef WORD_ANALYZER_H
 #define WORD_ANALYZER_H
 
-namespace sdds {
+namespace sdds
+{
     const int MAX_WORD_LEN = 21;
+    struct Word
+    {
+        char letters[MAX_WORD_LEN];
+        int count;
+    };
 
-    int readWord(char* word, FILE* fptr, int maxLen);
-    
+    int readWord(char *word, FILE *fptr, int maxLen);
+
     void programTitle();
-    void wordStats(const char* filename);
+    void wordStats(const char *filename);
 
 }
 
