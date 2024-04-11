5e8b15b0e2c5c3abaa686bb14e971e1b14db1f93
Jyle Vergara <jylevergara@gmail.com>
2024-04-11 10:08:38 -0400

    format endlist

--- WordAnalyzer.cpp
+++ WordAnalyzer.cpp
@@ -39,7 +39,8 @@ namespace sdds
         return foundIndex;
     }
 
-        void endList() {
+    void endList()
+    {
         cout << "------------------------------------------------" << endl;
     }
 
