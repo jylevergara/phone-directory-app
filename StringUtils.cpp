9b8b11262eb59c01415de42de00bb483bbb02468
Jyle Vergara <jylevergara@gmail.com>
2024-04-15 13:25:47 -0400

    format trim function

--- StringUtils.cpp
+++ StringUtils.cpp
@@ -1,17 +1,19 @@
 #include "StringUtils.h"
 
-namespace phonedirectory {
-
-
+namespace phonedirectory
+{
 
     // removes the non-alphabetic characters from the begining and end of a word
-    void trim(char word[]) {
+    void trim(char word[])
+    {
         int i;
-        while (word[0] && !isAlpha(word[0])) {
+        while (word[0] && !isAlpha(word[0]))
+        {
             strCpy(word, word + 1);
         }
         i = strLen(word);
-        while (i && !isAlpha(word[i - 1])) {
+        while (i && !isAlpha(word[i - 1]))
+        {
             word[i-- - 1] = 0;
         }
     }
