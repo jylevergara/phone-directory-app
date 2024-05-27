b019ba7adc5b7ee288f99bbf0f2dfc94ee63ffb9
Jyle Vergara <jylevergara@gmail.com>
2024-05-27 10:17:22 -0400

    rearrange function in wordanalyzer

--- WordAnalyzer.h
+++ WordAnalyzer.h
@@ -11,19 +11,18 @@ namespace phonedirectory
         int count;
     };
 
-    int readWord(char *word, FILE *fptr, int maxLen);
     int searchWords(const Word words[], int num, const char word[]);
-    int totalCount(const Word *words, int num);
-    void title(const char* value, int len);
-    int findMaxLen(const Word words[], int noOfWords);
-    void programTitle();
-    void wordStats(const char *filename);
     void addWord(Word words[], int *index, const char newWord[]);
+    int readWord(char *word, FILE *fptr, int maxLen);
+    void title(const char* value, int len);
+    void endList();
+    void totalWordsStat(int totNoWrds, int totalOverAll, int longestWord);
+    int findMaxLen(const Word words[], int noOfWords);
     void listWords(const Word words[], int noOfWords, const char *theTitle);
     void sort(Word words[], int cnt, int sortType);
-    void totalWordsStat(int totNoWrds, int totalOverAll, int longestWord);
-    void endList();
-
+    int totalCount(const Word *words, int num);
+    void wordStats(const char *filename);
+    void programTitle();
 }
 
 #endif
