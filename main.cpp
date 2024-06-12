5d894bdaa810d0082d59982bd66dabca6381afa8
Jyle Vergara <jylevergara@gmail.com>
2024-06-12 10:28:41 -0400

    write main function

--- main.cpp
+++ main.cpp
@@ -5,3 +5,12 @@ using namespace std;
 #include "StringUtils.h"
 #include "WordAnalyzer.h"
 using namespace phonedirectory;
+
+int main() {
+    char filename[256];
+    programTitle();
+    cout << "Enter filename: ";
+    cin >> filename;
+    wordStats(filename);
+    return 0;
+ }
